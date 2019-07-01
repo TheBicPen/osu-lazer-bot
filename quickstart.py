03c97582b6171a2ec3edb21d62d9c25d0e5d9a44
Alex <mashkal2000@gmail.com>
2019-07-01 03:30:10 -0400

    add regex link parser

--- quickstart.py
+++ quickstart.py
@@ -1,12 +1,26 @@
 import praw
 import io
+import re
 
-token_file = io.open("creds/token.txt", "r")
+token_file = io.open("creds/reddit_token.txt", "r")
 token = token_file.readlines()
 token_file.close()
+token[0] = token[0].rstrip('\n')
+
+link_re = re.compile("\[[^\].]*\]\([^\).]*\)")
+print(token)
 
 if len(token) > 0:
     reddit = praw.Reddit(client_id=token[0],
                      client_secret=token[1],
                      user_agent='my user agent')
-    print(reddit.user.me())
+    # print(reddit.user.me())
+
+    osugame = reddit.subreddit('osugame')
+    for submission in osugame.top('day',limit=5):
+        print("Title:" + submission.title)
+        comments = submission.comments.list()
+        for comment in comments:
+            if comment.author == 'osu-bot':
+                # print(comment.body)
+                print(link_re.findall(comment.body))
