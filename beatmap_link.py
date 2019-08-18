8d004b8c777a9bbc592e28769c7d80fac602b2ab
Alex <mashkal2000@gmail.com>
2019-08-18 13:16:25 -0400

    update requirements. remove old commented code. change main call to take all sys args

--- beatmap_link.py
+++ beatmap_link.py
@@ -15,31 +15,22 @@ def auth_official():
     try:
         with open('creds/payload', 'rb') as handle:
             payload = pickle.loads(handle.read())
-        r=s.get("https://osu.ppy.sh/forum/ucp.php?mode=login")
+        r = s.get("https://osu.ppy.sh/forum/ucp.php?mode=login")
     except Exception as e:
         return "Failed to obtain osu credentials: {0}".format(e)
-    # with open('responses/get_login.html', 'wb') as fd:
-    #     for chunk in r.iter_content(chunk_size=128):
-    #         fd.write(chunk)
-    r=s.post("https://osu.ppy.sh/forum/ucp.php?mode=login", data=payload)
-    # with open('responses/post_login.html', 'wb') as fd:
-    #     for chunk in r.iter_content(chunk_size=128):
-    #         fd.write(chunk)
+    r = s.post("https://osu.ppy.sh/forum/ucp.php?mode=login", data=payload)
     if not r.ok:
         with open('responses/post_login.html', 'wb') as fd:
             for chunk in r.iter_content(chunk_size=128):
                 fd.write(chunk)
         return "failed to authenticate:" + r.reason
-    # r=s.get("https://osu.ppy.sh/forum/index.php?success=1563054777")
-    # with open('responses/get_loggedin.html', 'wb') as fd:
-    #     for chunk in r.iter_content(chunk_size=128):
-    #         fd.write(chunk)
 
     # no link processing
 
     return s
 
-def get_digits(prepend:str, links: list):
+
+def get_digits(prepend: str, links: list):
     """
     Remove everything up to and including the last forward slash of each item in links
     and add the prefix prepend to it.
@@ -50,7 +41,8 @@ def get_digits(prepend:str, links: list):
     except Exception as e:
         print(e)
 
-def auth_bloodcat(links:list):
+
+def auth_bloodcat(links: list):
     """
     Return a session suitable for bloodcat (no auth required).
     Convert a link that to https://bloodacat.com/osu/s/<the original link's basename>
@@ -58,14 +50,15 @@ def auth_bloodcat(links:list):
     get_digits("https://bloodcat.com/osu/s/", links)
     return requests.Session()
 
-def download(auth_link, links:list, filetype:str):
+
+def download(auth_link, links: list, filetype: str):
     """    
     Authenticate and convert links with auth_link and download objects (typically beatmaps) at links.
     Each file is saved to 'responses/downloads/' with its name and being the text after the last '/'
     and the extension filetype.
     Return relative filenames of downloaded files.
     """
-    s=auth_link(links)
+    s = auth_link(links)
     print("Downloading links: ")
     print(links)
     stripped_links = []
@@ -75,7 +68,8 @@ def download(auth_link, links:list, filetype:str):
         if r.ok:
             try:
                 link_nums = link[link.rindex('/')+1:]
-                filename = 'responses/downloads/{0}.{1}'.format(link_nums, filetype)
+                filename = 'responses/downloads/{0}.{1}'.format(
+                    link_nums, filetype)
                 with open(filename, 'wb') as fd:
                     for chunk in r.iter_content(chunk_size=128):
                         fd.write(chunk)
@@ -84,46 +78,54 @@ def download(auth_link, links:list, filetype:str):
             except Exception as e:
                 print("Failed to write response content to file " + filename, e)
         else:
-            print("Request for {0} failed with reason {1}.".format(link, r.reason))
+            print("Request for {0} failed with reason {1}.".format(
+                link, r.reason))
     return stripped_links
 
-def download_replays(command:str, links:list):
+
+def download_replays(command: str, links: list):
     """
     Download replays by substituting the proper command arguments into command.
     Links is a list of lists. Each list descibes a play and contains 
     """
 
-def print_links(links:dict):
+
+def print_links(links: dict):
     """
     Print a dictionary, parsing its values as strings. 
     Return a string of every value in links appended.
     """
     if len(links) == 0:
         print("no links in posts")
-    out=""
+    out = ""
     print("Links in dict: ")
-    for post,urls in links.items():
-        # out = out+"https://osu.ppy.sh/d/"+urls[1] + "\n"
+    for post, urls in links.items():
         print(post + " " + str(urls))
     return out
 
-def get_beatmap_links(links:dict):
+
+def get_beatmap_links(links: dict):
     """
     Takes a dict of lists, and returns a list of links to the beatmaps.
     """
     out = []
-    for post,urls in links.items():
+    for post, urls in links.items():
         if len(urls) > 1:
             out.append(urls[1])
     return out
 
+
 def main(down_prog, down_args, max_plays):
-    post_to_links = fp.get_subreddit_links(fp.initialize(), 'osugame', 'top', 10, 'osu-bot') # get all osu-bot links
-    post_to_links = fp.parse_osu_links(post_to_links) # remove all links not pointing to osu.ppy.sh
-    while len(post_to_links) > int(max_plays):      # only get the top 2 most upvoted plays of the day
+    post_to_links = fp.get_subreddit_links(
+        fp.initialize(), 'osugame', 'top', 10, 'osu-bot')  # get all osu-bot links
+    # remove all links not pointing to osu.ppy.sh
+    post_to_links = fp.parse_osu_links(post_to_links)
+    # only get the top 2 most upvoted plays of the day
+    while len(post_to_links) > int(max_plays):
         post_to_links.popitem()
 
-    down_result=download(auth_bloodcat, get_beatmap_links(post_to_links), "osz")
+    down_result = download(
+        auth_bloodcat, get_beatmap_links(post_to_links), "osz")
     print("Download result: ")
     print(down_result)
 
@@ -141,11 +143,12 @@ def main(down_prog, down_args, max_plays):
         return None
 
     for post, links in post_to_links.items():
-        retcode = subprocess.call([down_prog, down_args, '--api-key', token, '--beatmap-id', links[0], \
-            '--user-id', links[4], '--output-file', 'responses/downloads/{0}-{1}.osr'.format(links[0], links[4])])
-        if retcode != 0: 
+        retcode = subprocess.call([down_prog, down_args, '--api-key', token, '--beatmap-id', links[0],
+                                   '--user-id', links[4], '--output-file', 'responses/downloads/{0}-{1}.osr'.format(links[0], links[4])])
+        if retcode != 0:
             return 1
+
 
 if __name__ == "__main__":
     print(sys.argv)
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(*sys.argv[1:])
--- fetch_plays.py
+++ fetch_plays.py
@@ -6,33 +6,30 @@ import re
 osu_link = "(http|https)://osu\.ppy\.sh/./\d*"
 link_re = re.compile('\[.*?\]\(.*?\)')
 url_re = re.compile('\(.*?\)')
-# print(token)
 
 
-def get_subreddit_links(reddit:praw.Reddit, subreddit:str, sort_type:str, num_posts:int, author:str):
+def get_subreddit_links(reddit: praw.Reddit, subreddit: str, sort_type: str, num_posts: int, author: str):
     """
     Uses the Reddit instance to retrieve the first num_posts from subreddit, sorted by sort_type.
     Then searches for comments on these posts made by author (should be a bot that posts on all posts),
     and finally returns a list of lists of markdown-formatted links on that post by the given author.
     Only the first comment by author on each post is considered. 
-    
+
     Only the first set of comments is loaded, so the comment by author should be pinned or highly upvoted.
     See praw api docs for list of valid sort types - https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html 
     """
     subreddit = reddit.subreddit(subreddit)
-    link_set={}
+    link_set = {}
     if sort_type == 'hot':
         submissions = subreddit.hot(limit=num_posts)
     elif sort_type == 'top':
-        submissions = subreddit.top('day',limit=num_posts)
+        submissions = subreddit.top('day', limit=num_posts)
     for submission in submissions:
-        # print("Title: " + submission.title)
         comments = submission.comments.list()
-        link_list=[] #heh
+        link_list = []  # heh
         try:
             for comment in comments:
                 if comment.author == author:
-                    # print(comment.body)
                     links = link_re.findall(comment.body)
                     for link in links:
                         link_list.append(link)
@@ -43,53 +40,24 @@ def get_subreddit_links(reddit:praw.Reddit, subreddit:str, sort_type:str, num_po
     return link_set
 
 
-def parse_osu_links(d:dict):
+def parse_osu_links(d: dict):
     """
     Parse a dict of markdown-links sent by osu-bot. Extract the osu.ppy.sh URLs,
     and return a dict where keys are unchanged, but the values are the URLs.
-    
+
     link format: beatmap, download, mapper, top_on_map, player, top_play_of_player
     """
-    # out_str = "title;beatmap;download;mapper;top_on_map;player;top_play_of_player\n"
     new_d = {}
-    for key,value in d.items():
+    for key, value in d.items():
         if value != []:
-            # out_str = "{0}\n\"{1}\";".format(out_str,key)
             new_val = []
-            # beatmap, download, mapper, player, top_on_map, top_play_of_top_on_map = ""
             for link in value:
-                # if "osu.ppy.sh/b/" in link and beatmap == "":
-                #     beatmap = url_re.search(link).group(0)
-                # elif "osu.ppy.sh/d/" in link and download == "":
-                #     download = url_re.search(link).group(0)
-                # elif "osu.ppy.sh/u/" in link and mapper == "":
-                #     mapper = url_re.search(link).group(0)
-                # elif "osu.ppy.sh/u/" in link and player == "":
-                #     player = url_re.search(link).group(0)
-                # elif "osu.ppy.sh/u/" in link and top_on_map == "":
-                #     top_on_map = url_re.search(link).group(0)
-                # elif "osu.ppy.sh/b/" in link and top_play_of_top_on_map == "":
-                #     top_play_of_top_on_map = url_re.search(link).group(0)
                 match = re.search(osu_link, link)
                 if match is not None:
-                    # out_str = "{0}{1};".format(out_str,re.sub("osu.ppy.sh/", "", match.group(0)))
-                    # new_val.append(re.sub("osu.ppy.sh/./", "", match.group(0)))
                     new_val.append(match.group(0))
             new_d[key] = new_val
-            # out_str = out_str + "\n"
-        # out_str.append("{0},{1},{2},{3},{4},{5}\n".format(beatmap, download, mapper, player, top_on_map, top_play_of_top_on_map))
     return new_d
-    # return out_str # am rarted
 
-    # out ={}
-    # for key,value in d.items():
-    #     if value != []:
-    #         out_value=[]
-    #         for link in value:
-    #             link = link[link.rfind('(')+1:-1]
-    #             out_value.append(link)
-    #     out[key] = value
-    # return out
 
 def initialize():
     """
@@ -105,15 +73,14 @@ def initialize():
         return None
     try:
         reddit = praw.Reddit(client_id=token[0],
-                        client_secret=token[1],
-                        user_agent='my user agent')
-        # print(reddit.user.me())
+                             client_secret=token[1],
+                             user_agent='my user agent')
         return reddit
     except:
         print("unable to initialize Reddit instance")
 
+
 if __name__ == "__main__":
-    plays_to_linkset = get_subreddit_links(initialize(), 'osugame', 'top', 5, 'osu-bot')
-    # print(links)
-    # print('\n\n\n')
+    plays_to_linkset = get_subreddit_links(
+        initialize(), 'osugame', 'top', 5, 'osu-bot')
     print(parse_osu_links(plays_to_linkset))
