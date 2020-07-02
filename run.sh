20e9c633da973695cb512524eeef72ce521ebad6
bic <mashkal2000@gmail.com>
2020-07-02 13:05:04 -0700

    Merge runner scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,60 @@
+#!/bin/bash
+
+run=0
+download='replays beatmaps'
+sort='-hot'
+num_check='8'
+helper='node osu-replay-downloader/fetch.js'
+
+while [ $# -gt 0 ]; do
+     case "$1" in
+     -hot | -hour | -day | -week | -month | -year | -all)
+          sort="$1"
+          ;;
+     -[0-9])
+          num_check="$1"
+          ;;
+     -run)
+          run=1
+          ;;
+     -beatmaps)
+          download='beatmaps'
+          ;;
+     -replays)
+          download='replays'
+          ;;
+     -none)
+          download=''
+          ;;
+     -download-all)
+          download='beatmaps replays'
+          ;;
+     -*)
+          echo invalid option: "$1"
+          ;;
+     esac
+     shift
+done
+
+# remove '-' prefixes
+sort=$(echo "$sort" | tr -d -)
+num_check=$(echo "$num_check" | tr -d -)
+
+cd "$(dirname $0)"
 . "./env/bin/activate"
-python beatmap_link.py "replays beatmaps" "node osu-replay-downloader/fetch.js" 8 hot bloodcat
+
+python beatmap_link.py "$download" "$helper" "$num_check" "$sort" || exit 1
+files=""
+for file in $(
+     find $(pwd)/downloads/ -type f -iname '*.osz'
+     find $(pwd)/downloads/ -type f -iname '*.osr'
+); do
+     echo file="$file"
+     files="$files $file"
+done
+
+if [ $run = 1 ]; then
+     $(<creds/osu_path) $files
+else 
+     echo 'Skipping launching osu!'
+fi
